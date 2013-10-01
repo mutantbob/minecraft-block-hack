39dee714276e1c6c446ee7f69077d0ab8c82d6c0
Robert Forsman <git@thoth.purplefrog.com>
2013-10-01 08:42:31 -0400

    New FatFace object.

--- nbt-exp/src/main/java/com/purplefrog/minecraftExplorer/BlenderMeshElement.java
+++ nbt-exp/src/main/java/com/purplefrog/minecraftExplorer/BlenderMeshElement.java
@@ -9,11 +9,11 @@ package com.purplefrog.minecraftExplorer;
  */
 public abstract class BlenderMeshElement
 {
-    public final int x;
-    public final int y;
-    public final int z;
-    public final int bt;
-    public final int blockData;
+    public int x;
+    public int y;
+    public int z;
+    public int bt;
+    public int blockData;
 
     public BlenderMeshElement(int x, int y, int z, int bt, int blockData)
     {
@@ -36,6 +36,27 @@ public abstract class BlenderMeshElement
     }
 
     public abstract String asPython();
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (o instanceof BlenderMeshElement) {
+            BlenderMeshElement arg = (BlenderMeshElement) o;
+            return equals_(arg);
+        } else {
+            return false;
+        }
+    }
+
+    public boolean equals_(BlenderMeshElement arg)
+    {
+        return arg.x == x
+            && arg.y == y
+            && arg.z == z
+            && arg.bt == bt
+            && arg.blockData == blockData;
+    }
+//
 
     public static class Pane
         extends BlenderMeshElement
@@ -60,13 +81,18 @@ public abstract class BlenderMeshElement
     public static class Face
         extends BlenderMeshElement
     {
-        private final FaceSide orientation;
+        public final FaceSide orientation;
 
         public Face(int x, int y, int z, int bt, int blockData, FaceSide orientation)
         {
             super(x, y, z, bt, blockData);
 
             this.orientation = orientation;
+        }
+
+        public Face(int x, int y, int z, BlockPlusData blockData, FaceSide orientation)
+        {
+            this(x,y,z,blockData.blockType, blockData.data, orientation);
         }
 
         @Override
@@ -90,6 +116,53 @@ public abstract class BlenderMeshElement
         public String asPython()
         {
             return ("minecraftWidget("+locString()+", "+bt+","+blockData+")");
+        }
+    }
+
+    public static class FatFace
+        extends BlenderMeshElement
+    {
+        public final FaceSide orientation;
+        public int du;
+        public int dv;
+
+        public FatFace(int x, int y, int z, int bt, int blockData, FaceSide orientation, int du, int dv)
+        {
+            super(x, y, z, bt, blockData);
+
+            this.orientation = orientation;
+            this.du = du;
+            this.dv = dv;
+        }
+
+        public FatFace(Face f1)
+        {
+            this(f1.x, f1.y, f1.z, f1.bt, f1.blockData, f1.orientation, 1,1);
+        }
+
+        public FatFace(FatFace arg)
+        {
+            this(arg.x, arg.y, arg.z, arg.bt, arg.blockData, arg.orientation, arg.du, arg.dv);
+        }
+
+        @Override
+        public String asPython()
+        {
+            return "minecraftFatFace("+locString()+", "+bt+","+blockData+", " +orientation+", "+du+","+dv+ ")";
+        }
+
+        @Override
+        public boolean equals(Object o)
+        {
+            if (o instanceof FatFace) {
+                FatFace arg = (FatFace) o;
+                return super.equals(arg)
+                    && arg.orientation == orientation
+                    && arg.du == du
+                    && arg.dv == dv;
+            } else {
+                return false;
+            }
         }
     }
 }
