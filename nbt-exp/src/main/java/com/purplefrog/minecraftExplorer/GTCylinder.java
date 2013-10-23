cdeb9dafd5df0f3559c78dfa98cde44b7ee21cc2
Robert Forsman <git@thoth.purplefrog.com>
2013-10-23 13:18:49 -0400

    Extract the segment math into its own LineMath class.

--- nbt-exp/src/main/java/com/purplefrog/minecraftExplorer/GTCylinder.java
+++ nbt-exp/src/main/java/com/purplefrog/minecraftExplorer/GTCylinder.java
@@ -10,25 +10,21 @@ import com.purplefrog.jwavefrontobj.*;
  * To change this template use File | Settings | File Templates.
  */
 public class GTCylinder
+    extends LineMath
     implements GeometryTree
 {
-    private final Point3D origin;
-    private final Point3D axis;
     private final double radius;
     private final GeometryTree inside;
     private final GeometryTree outside;
-    private final double axisL2;
 
     public GTCylinder(Point3D origin, Point3D axis, double radius, GeometryTree inside, GeometryTree outside)
     {
+        super(origin, axis);
 
-        this.origin = origin;
-        this.axis = axis;
         this.radius = radius;
         this.inside = notNull(inside);
         this.outside = notNull(outside);
 
-        axisL2 = GTEllipse.l2(axis.x, axis.y, axis.z);
     }
 
     public static GeometryTree notNull(GeometryTree tree)
@@ -39,25 +35,12 @@ public class GTCylinder
     @Override
     public BlockPlusData pickFor(int x, int y, int z)
     {
-        double x1 = x - origin.x;
-        double y1 = y - origin.y;
-        double z1 = z - origin.z;
-        double dot = x1 *axis.x
-            + y1 *axis.y
-            + z1 *axis.z;
+        Squares sq = getSquares(x, y, z);
 
-        double c2 = l2(x1, y1, z1);
-        double a2 = dot*dot/axisL2;
-
-        double b2 = c2-a2;
-
-        boolean out = b2 > radius*radius;
+        boolean out = sq.b2 > radius*radius;
 
         return (out ? outside : inside).pickFor(x, y, z) ;
     }
 
-    public static double l2(double x, double y, double z)
-    {
-        return GTEllipse.l2(x,y,z);
-    }
+
 }
---
+++ nbt-exp/src/main/java/com/purplefrog/minecraftExplorer/LineMath.java
@@ -0,0 +1,91 @@
+package com.purplefrog.minecraftExplorer;
+
+import com.purplefrog.jwavefrontobj.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: thoth
+ * Date: 10/21/13
+ * Time: 4:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LineMath
+{
+    protected final Point3D origin;
+    protected final Point3D axis;
+    protected final double axisL22;
+    public final double axisL2;
+
+    public LineMath(Point3D origin, Point3D axis)
+    {
+        axisL22 = GTEllipse.l2(axis.x, axis.y, axis.z);
+        axisL2 = Math.sqrt(axisL22);
+        this.origin = origin;
+        this.axis = axis;
+    }
+
+    public static double l2(double x, double y, double z)
+    {
+        return GTEllipse.l2(x,y,z);
+    }
+
+    public Squares getSquares(double x, double y, double z)
+    {
+        double x1 = x - origin.x;
+        double y1 = y - origin.y;
+        double z1 = z - origin.z;
+        double dot = x1 *axis.x
+            + y1 *axis.y
+            + z1 *axis.z;
+
+        double a = dot / axisL2;
+
+        double c2 = l2(x1, y1, z1);
+        double a2 = dot*dot/ axisL22;
+
+        double b2 = c2-a2;
+
+        return new Squares(a2, b2, c2, a);
+    }
+
+    public static LineMath fromEndpoints(Point3D origin, Point3D endpoint)
+    {
+        Point3D axis = new Point3D(endpoint.x - origin.x,
+            endpoint.y - origin.y,
+            endpoint.z - origin.z);
+        return new LineMath(origin, axis);
+    }
+
+    /**
+     * This stores the squares of the lengths of the sides of a right triangle formed by the origin, axis,
+     * and some point of interest.
+     */
+    public static class Squares
+    {
+        /**
+         *The square of the length of the side of the triangle aligned with the axis.
+         */
+        public double a2;
+        /**
+         * The square of the distance from the point of interest to the line segment.
+         */
+        public double b2;
+        /**
+         * The square of distance from the point of interest to the origin.
+         */
+        public double c2;
+        /**
+         * the length of the side of the triangle aligned with the axis.
+         * This can be negative if it's on the other side of the origin point.
+         */
+        public double a;
+
+        public Squares(double a2, double b2, double c2, double a)
+        {
+            this.a2 = a2;
+            this.b2 = b2;
+            this.c2 = c2;
+            this.a = a;
+        }
+    }
+}
