2b9ab3825b1f974eac6b4f1e18437d3ff1202004
Robert Forsman <git@thoth.purplefrog.com>
2013-10-29 14:12:09 -0400

    New scaledSum() and mix() vector functions.

--- nbt-exp/src/main/java/com/purplefrog/minecraftExplorer/Math2.java
+++ nbt-exp/src/main/java/com/purplefrog/minecraftExplorer/Math2.java
@@ -1,5 +1,7 @@
 package com.purplefrog.minecraftExplorer;
 
+import com.purplefrog.jwavefrontobj.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: thoth
@@ -27,5 +29,17 @@ public class Math2
     public static double L22(double dx, double dy, double dz)
     {
         return dx*dx + dy*dy + dz*dz;
+    }
+
+    public static Point3D scaledSum(Point3D A, double aF, Point3D B, double bF)
+    {
+        return new Point3D(aF *A.x+B.x* bF,
+            aF *A.y+B.y* bF,
+            aF *A.z+B.z* bF);
+    }
+
+    public static Point3D mix(Point3D p1, Point3D p2, double t)
+    {
+        return scaledSum(p1, 1- t, p2, t);
     }
 }
