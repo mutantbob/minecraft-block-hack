2f07a24a82b91a094b88fbb5ecd53755fdad31da
Robert Forsman <git@thoth.purplefrog.com>
2014-09-22 14:02:43 -0400

    Scan blocks with Y as the slowest axis.

--- nbt-exp/src/main/java/com/purplefrog/minecraftExplorer/ExportWebGL.java
+++ nbt-exp/src/main/java/com/purplefrog/minecraftExplorer/ExportWebGL.java
@@ -50,8 +50,8 @@ public class ExportWebGL
         List<BlenderMeshElement> polys = new ArrayList<BlenderMeshElement>();
 
         if (true) {
-            for (int x = x1; x<=x2; x++) {
-                for (int y=y1; y<=y2; y++) {
+            for (int y=y1; y<=y2; y++) {
+                for (int x = x1; x<=x2; x++) {
                     for (int z=z1; z<=z2; z++) {
                         editor.getBlenderMeshElements(polys, x,y,z);
                     }
