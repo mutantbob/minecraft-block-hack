490be3c09cc9d9ba4d6dea4859b7528782bed3d8
Robert Forsman <git@thoth.purplefrog.com>
2014-10-15 15:59:10 -0400

    move the sin/cosine calculation into the constructor, and add some special cases for multiples of 90

--- nbt-exp/src/main/java/com/purplefrog/minecraftExplorer/blockmodels/FaceSpec.java
+++ nbt-exp/src/main/java/com/purplefrog/minecraftExplorer/blockmodels/FaceSpec.java
@@ -10,6 +10,8 @@ public class FaceSpec
     public final String textureName;
     public final String cullface;
     public final double rotate;
+    private final double sine;
+    private final double cosine;
 
     public FaceSpec(String textureName, String cullface, double rotate)
     {
@@ -17,6 +19,23 @@ public class FaceSpec
         this.textureName = textureName;
         this.cullface = cullface;
         this.rotate = rotate;
+
+        if (rotate==0) {
+            sine=0;
+            cosine=1;
+        } else if (rotate==90) {
+            sine=1;
+            cosine=0;
+        } else if (rotate==180) {
+            sine=0;
+            cosine=-1;
+        } else if (rotate==270) {
+            sine=-1;
+            cosine=0;
+        } else {
+            sine = Math.sin(rotate * 3.14159 / 180);
+            cosine = Math.cos(rotate * 3.14159 / 180);
+        }
     }
 
     public static FaceSpec parse(JSONObject spec)
@@ -39,8 +58,6 @@ public class FaceSpec
             double x0 = uvSquare[i]-0.5;
             double y0 = uvSquare[i+1]-0.5;
 
-            double sine = Math.sin(rotate * 3.14159 / 180);
-            double cosine = Math.cos(rotate * 3.14159 / 180);
             double x2 = x0*cosine - y0*sine;
             double y2 = y0*cosine + x0*sine;
 
