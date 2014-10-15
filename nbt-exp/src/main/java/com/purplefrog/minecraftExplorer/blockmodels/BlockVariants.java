4c6e1347fe5eda2259c8b3b87c4ecf095c8c7137
Robert Forsman <git@thoth.purplefrog.com>
2014-10-15 12:23:29 -0400

    Try to match some variants (trap door, torch, stair, stems) with varying degrees of success

--- nbt-exp/src/main/java/com/purplefrog/minecraftExplorer/blockmodels/BlockVariants.java
+++ nbt-exp/src/main/java/com/purplefrog/minecraftExplorer/blockmodels/BlockVariants.java
@@ -1,5 +1,7 @@
 package com.purplefrog.minecraftExplorer.blockmodels;
 
+import com.purplefrog.minecraftExplorer.*;
+
 import java.util.*;
 
 /**
@@ -16,18 +18,93 @@ public class BlockVariants
 
     public static Random rand = new Random();
 
+    public static String[] torch_facings = {
+        "down",
+        "east",
+        "west",
+        "south",
+        "north",
+        "up"
+    };
+
     public OneBlockModel getVariant(int blockType, int blocKData)
     {
 
-        List<OneBlockModel> x=null;
-        
-//        x = variants.get("normal");
+        List<OneBlockModel> x = matchVariant(blockType, blocKData);
+
         if (null==x) {
+
+
             // XXX not right
-            x = variants.get(0).model;
+            x = variants.get(variants.size()-1).model;
         }
 
         int idx = rand.nextInt(x.size());
         return x.get(idx);
+    }
+
+    public List<OneBlockModel> matchVariant(int blockType, int blocKData)
+    {
+//        if (true)
+//            return null;
+
+        for (OneBlockModel.Named variant : variants) {
+            if (variant.name.startsWith("facing=")) {
+                if (variant.name.equals("facing="+ facingName(blockType, blocKData))) {
+                    return variant.model;
+                }
+            }
+        }
+
+        if (variants.size()==1 && variants.get(0).name.equals("normal"))
+            return null;
+
+        return null;
+    }
+
+    public static String facingName(int blockType, int blockData)
+    {
+        if (blockType == BlockDatabase.BLOCK_TYPE_TORCH) {
+            return getOrNull(blockData, torch_facings);
+        } else if (blockType == BlockDatabase.BLOCK_TYPE_LADDER) {
+            return getOrNull(blockData, null, null, "north", "south", "west", "east");
+        } else if (blockType == BlockDatabase.BLOCK_TYPE_STONE_BRICK_STAIRS
+            || blockType == BlockDatabase.BLOCK_TYPE_QUARTZ_STAIRS) {
+            return getOrNull(blockData&3, "east", "west", "south", "north");
+        } else if (blockType == BlockDatabase.BLOCK_TYPE_PUMPKIN) {
+            return getOrNull(blockData, "south", "west", "north", "east");
+        } else if (blockType == BlockDatabase.BLOCK_TYPE_PUMPKIN_STEM
+            ||blockType == BlockDatabase.BLOCK_TYPE_MELON_STEM) {
+            return "south"; // XXX wrong
+        } else if (blockType == BlockDatabase.BLOCK_TYPE_TRAPDOOR) {
+            return getOrNull(blockData, "south,half=bottom,open=false",
+                "north,half=bottom,open=false",
+                "east,half=bottom,open=false",
+                "west,half=bottom,open=false",
+                "south,half=bottom,open=true",
+                "north,half=bottom,open=true",
+                "east,half=bottom,open=true",
+                "west,half=bottom,open=true",
+                "south,half=top,open=false",
+                "north,half=top,open=false",
+                "east,half=top,open=false",
+                "west,half=top,open=false",
+                "south,half=top,open=true",
+                "north,half=top,open=true",
+                "east,half=top,open=true",
+                "west,half=top,open=true");
+        } else if (blockType==26) {
+            // XXX
+            return null;
+        }
+
+        return null;
+    }
+
+    public static <T> T getOrNull(int idx, T... array)
+    {
+        if (idx < array.length)
+            return array[idx];
+        return null;
     }
 }
