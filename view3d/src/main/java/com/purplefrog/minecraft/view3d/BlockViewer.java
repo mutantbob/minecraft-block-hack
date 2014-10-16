65b49e43d9b6de4380f9bff1d4d627300f7e7b75
Robert Forsman <git@thoth.purplefrog.com>
2014-10-16 11:50:13 -0400

    get rid of the SphereDemo experiment for Java3D

--- view3d/src/main/java/com/purplefrog/minecraft/view3d/BlockViewer.java
+++ view3d/src/main/java/com/purplefrog/minecraft/view3d/BlockViewer.java
@@ -1,17 +1,12 @@
 package com.purplefrog.minecraft.view3d;
 
 import com.jogamp.opengl.util.*;
-import com.sun.j3d.utils.geometry.*;
-import com.sun.j3d.utils.universe.*;
-import org.apache.log4j.*;
 import org.json.*;
 
-import javax.media.j3d.*;
 import javax.media.opengl.*;
 import javax.media.opengl.awt.*;
 import javax.media.opengl.glu.*;
 import javax.swing.*;
-import javax.vecmath.*;
 import java.awt.*;
 import java.io.*;
 
@@ -177,41 +172,5 @@ public class BlockViewer
 
     public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean
         deviceChanged) {}
-
-
-    public static class SphereDemo {
-        public SphereDemo()
-        {
-            SimpleUniverse universe = new SimpleUniverse();
-
-            BranchGroup group = new BranchGroup();
-
-            Sphere sphere = new Sphere(0.3f);
-            group.addChild(sphere);
-
-            Color3f light1Color = new Color3f(1.8f, 0.1f, 0.1f);
-
-            BoundingSphere bounds =
-
-                new BoundingSphere(new Point3d(0.0,0.0,0.0), 100.0);
-
-            Vector3f light1Direction = new Vector3f(4.0f, -7.0f, -12.0f);
-
-            DirectionalLight light1
-
-                = new DirectionalLight(light1Color, light1Direction);
-
-            light1.setInfluencingBounds(bounds);
-
-            group.addChild(light1);        universe.getViewingPlatform().setNominalViewingTransform();
-
-            universe.addBranchGraph(group);
-        }
-
-        public static void main(String[] argv)
-        {
-            new SphereDemo();
-        }
-    }
 
 }
